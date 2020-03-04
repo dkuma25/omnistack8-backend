3cd5ae4b117433ecc22589ae1db3ccf4bf8cd2ff
Gabriel Gomes <bibiel2605@gmail.com>
2020-03-04 15:44:54 -0300

    Models, Controllers, cors

--- src/Server.js
+++ src/Server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const cors = require('cors')
 
 // mongoose.connect('mongodb+srv://omnistack8:omnistack8@cluster0-lmjul.mongodb.net/omnistack8?retryWrites=true&w=majority', {
 //   useNewUrlParser: true,
@@ -14,6 +15,7 @@ mongoose.connect('mongodb://localhost:27017/omnistack8', {
 
 const server = express()
 
+server.use(cors())
 server.use(express.json())
 server.use(routes)
 
---
+++ src/controllers/LikeController.js
@@ -0,0 +1,26 @@
+const Dev = require('../models/Dev')
+
+module.exports = {
+  async store(req, res) {
+    const { user } = req.headers
+    const { devId } = req.params
+
+    const loggedDev = await Dev.findById(user)
+    const targetDev = await Dev.findById(devId)
+
+    if (!targetDev) {
+      return res.status(400).send({ error: 'Dev not exists.' })
+    } else if (!loggedDev) {
+      return res.status(400).send({ error: 'Dev not logged.' })
+    }
+    if (!loggedDev.likes.includes(targetDev._id)){
+      loggedDev.likes.push(targetDev._id)
+      await loggedDev.save()
+    }
+    if (targetDev.likes.includes(loggedDev._id)) {
+      console.log("DEU MATCH!")
+    }
+
+    return res.json(loggedDev)
+  }
+}
